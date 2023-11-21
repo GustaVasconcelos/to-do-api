d4ca0dcda895260cab208cda2f9e1a6a1cf73c60
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-11-20 22:00:25 -0300

    feat: adicionando teste

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.get("/", (req, res) => {
+    res.send("teste");
+})
+
 app.listen(porta, () => {
     console.log('Servidor executando na porta: ' + porta)
 });
