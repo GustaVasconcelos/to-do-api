23c8bb0305c5821ab8afc3ab300f8e55df13d276
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-11-20 21:11:50 -0300

    feat: adicionando cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express'
+import cors from 'cors';
+import express from 'express';
 import RepositorioUsuario from './external/db/repositorioUsuario';
 import Db from './external/db/db';
 import SenhaCripto from './external/auth/senhaCripto';
@@ -12,6 +13,7 @@ import ValidarUsuario from './external/validations/validarUsuario';
 const app = express();
 const porta = process.env.API_PORT ?? 4000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
